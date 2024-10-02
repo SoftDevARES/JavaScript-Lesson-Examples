aa8ef5e3b0d9402c82e9526e61325a5c7031508e
Furkan TÜRKER <furkanturkertr@gmail.com>
2024-10-02 20:21:20 +0300

    Diziler

---
+++ Dizilere(Array)/app.js
@@ -0,0 +1,26 @@
+//Diziler
+
+/*  let sayilar = [0,1,2,3,4,5,"ali",6];
+
+alert (sayilar[6]);
+
+*/
+
+
+/*  
+let dizi1 = ["Enes", "Ali"];
+let dizi2 = new Array("Enes", "Ali");
+
+*/
+
+
+
+//FOREACH Döngüsü
+
+// Dizlerde kullandığımız döngü.
+
+let isimler = ["furkan", "gurkan", "ali"];
+
+isimler.forEach(function(isim) {
+    document.write(" " + isim)
+});
