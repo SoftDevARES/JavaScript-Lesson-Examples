435ff5416b8a6e81a2b958fcf4f1de88e74ae8ad
Furkan Türker <engineerfurkanturker@gmail.com>
2024-11-19 20:15:12 +0300

    Çatışmalar çözüldü

--- TodoList Projesi/app.js
+++ TodoList Projesi/app.js
@@ -12,11 +12,23 @@ runEvents();
 
 function runEvents() {
     form.addEventListener("submit", addTodo);
+<<<<<<< HEAD
+=======
+    document.addEventListener("DOMContentLoaded", pageLoaded);
+}
+
+function pageLoaded(){
+    checkFormTodosStorage();
+    todos.forEach(function(todo){
+        addTodoToUI(todo);
+    });
+>>>>>>> 7278a1c (Initial commit)
 }
 
 function addTodo(e) {
     const inputText = addinput.value.trim();
     if (inputText === "") {
+<<<<<<< HEAD
         alert("Bir değer giriniz");
     } else {
         // arayüze ekleme
@@ -27,6 +39,19 @@ function addTodo(e) {
 }
 
 function addTodoarayüz(newtodo) {
+=======
+        showAlert("warning", "Lütfen Boş Brakmayınız");
+    } else {
+        // arayüze ekleme
+        addTodoToUI(inputText);
+        addTodoSorage(inputText);
+        showAlert("success", "Todo Eklendi.");
+    }
+    e.preventDefault(); // Sayfanın yenilenmesini engeller
+}
+
+function addTodoToUI(newtodo) {
+>>>>>>> 7278a1c (Initial commit)
     // <li class="list-group-item d-flex justify-content-between">Todo 1
     //                         <a href="#" class="delete-item">
     //                             <i class="fa fa-remove"></i>
@@ -64,3 +89,22 @@ function checkFormTodosStorage() {
         todos = JSON.parse(localStorage.getItem("todos"));
     }
 }
+<<<<<<< HEAD
+=======
+
+function showAlert(type,message){
+    /*
+    <div class="alert alert-warning" rolse="alert">
+    Todo lİST Eklendi
+    >/div> */
+
+    const div = document.createElement("div");
+    div.className="alert alert-"+type;
+    div.textContent = message;
+    firstcordbody.appendChild(div);
+
+    setTimeout(function() {
+        div.remove();
+    }, 2500);
+}
+>>>>>>> 7278a1c (Initial commit)
