0154a50d26d5ac475b90ed7046f747e87d45da7c
Furkan Türker <furkanturker.dev@gmail.com>
2024-12-03 00:48:31 +0300

    BKİ

--- Uygulamalar/BedenKitle/app.js
+++ Uygulamalar/BedenKitle/app.js
@@ -1,8 +1,25 @@
-function calc(){
+function calc() {
     let bki;
     let category = document.getElementById("category");
-    let result= document.getElementById("result");
+    let result = document.getElementById("result");
 
-    let kilo = document.getElementById("kilo".value);
-    document.getElementById("kilo").textContent = kilo + "kg";
+    let kilo = parseInt(document.getElementById("kilo").value);
+    document.getElementById("kiloVal").textContent = kilo + "kg";
+
+    let boy = parseInt(document.getElementById("boy").value);
+    document.getElementById("boyVal").textContent = boy + "cm";
+
+    bki = (kilo / Math.pow((boy / 100), 2)).toFixed();
+    result.textContent = bki;   // mat.pow üssünü al demek 1.63 üsüü 1.63 mesela
+
+    if (bki <= 18.5) {
+        category.textContent = "Zayıfsın"
+        category.style.color = "orange"
+    } else if (bki >= 18.5 && bki <= 24) {
+        category.textContent = "Normal"
+        category.style.color = "green"
+    } else {
+        category.textContent = "Kilolu"
+        category.style.color = "red"
+    }
 }
