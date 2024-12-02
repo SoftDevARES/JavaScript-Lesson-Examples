ec9d08414949cadd6292f48d1a83caa267b1cad9
Furkan Türker <furkanturker.dev@gmail.com>
2024-12-03 00:29:43 +0300

    BEDEN kİTLE eNDEKSİ

---
+++ Uygulamalar/BedenKitle/app.js
@@ -0,0 +1,8 @@
+function calc(){
+    let bki;
+    let category = document.getElementById("category");
+    let result= document.getElementById("result");
+
+    let kilo = document.getElementById("kilo".value);
+    document.getElementById("kilo").textContent = kilo + "kg";
+}
