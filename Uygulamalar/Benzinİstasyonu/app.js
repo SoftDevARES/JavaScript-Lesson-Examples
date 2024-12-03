5626d760599f95e5bf00d473c2be7d45b31f13bb
Furkan Türker <furkanturker.dev@gmail.com>
2024-12-03 17:49:50 +0300

    BenzinUygulaması

---
+++ Uygulamalar/Benzinİstasyonu/app.js
@@ -0,0 +1,17 @@
+let dizel = 40.53, benzin = 44.40, lpg = 23.33;
+const yenisatır = "\r\n";
+
+const yakitmetni = "1- Dizel" + yenisatır + "benzin" + yenisatır + "lpg"
++ yenisatir + "Yakıt Türünü Seç";
+
+let yakittipi = prompt(yakitmetni);
+let ylitrre = Number(prompt("Yakıt Litresi"));
+let bakiye = Number(prompt("Bakiyenizi Giriniz"));
+
+if (yakittipi == "1"){
+
+}else if (yakittipi == "2"){
+
+}else{
+
+}
