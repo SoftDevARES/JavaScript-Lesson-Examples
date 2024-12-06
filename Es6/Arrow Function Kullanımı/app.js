eda418ea35595557a34859549c640c1407677ec9
Furkan Türker <furkanturker.dev@gmail.com>
2024-12-07 00:45:54 +0300

    Arrow fONKSİYONU

---
+++ Es6/Arrow Function Kullanımı/app.js
@@ -0,0 +1,30 @@
+// function yazdir(){
+//     console.log("Merhaba");
+// }
+
+// yazdir();
+
+// Arrow function
+
+// const yazdir = ()=>{
+//     console.log("Merhaba");
+// }
+
+// yazdir();
+
+// const yazdir = (ad, soyad)=>{
+//     console.log("Merhaba", ad, soyad);
+//     let
+// }
+
+// yazdir("furkan","türker");
+
+// const yazdir = (ad,soyad)=> console.log("merhaba",ad,soyad);
+
+// yazdir("furkan","türker");
+
+
+const kupal = x=> x*x*x
+
+
+console.log("Değer", kupal(3));
