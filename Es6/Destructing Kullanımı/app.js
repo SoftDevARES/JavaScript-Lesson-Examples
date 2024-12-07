b9f6653d7b1fba06b4283a5ab18130ca77120d2c
Furkan Türker <furkanturker.dev@gmail.com>
2024-12-08 00:08:05 +0300

    Javascript

--- Es6/Destructing Kullanımı/app.js
+++ Es6/Destructing Kullanımı/app.js
@@ -1,9 +1,23 @@
-let langs = ["c#", "c^+"]
-let langs1, langs2
+// let langs = ["c#", "c^+"]
+// let langs1, langs2
 
-// langs1 = langs[0]
-// langs2 = langs[1]
+// // langs1 = langs[0]
+// // langs2 = langs[1]
 
-[langs1, langs2] = langs
+// [langs1, langs2] = langs
 
-console.log(langs1, langs2)
+// console.log(langs1, langs2)
+
+const hesapla = (a,b) =>{
+    const toplam = a+b
+    const cikart = a-b
+    const carp = a*b
+    const bol = a/b
+
+    const dizi = [toplam,   cikart, carp, bol]
+    return dizi
+}
+
+let [a, b ,c ,d] = hesapla(10,2)
+
+console.log(a,b,c,d)
